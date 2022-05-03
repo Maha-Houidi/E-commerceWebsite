5d5d3ed82650d6730f0fb05a2e58674bac9100e6
Maha-Houidi <maha.houidi@etudiant-isi.utm.tn>
2022-05-03 21:32:15 +0200

    admin page

---
+++ admin/navbar-admin.php
@@ -0,0 +1,24 @@
+<nav class="navigation">
+
+	<!-- Logo -->
+	<div class="logo">
+		<h1>PARFUMERIE</h1>
+	</div>
+			
+	<!-- Navigation -->
+	<ul class="menu-list">
+        <li><a href="g-produits.php">Admin</a></li>
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Products</a></li>
+		<li><a href="#">Cart</a></li>
+		<li><a href="#">Order</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="../logout.inc.php">Logout</a></li>
+	</ul>
+
+	<div class="humbarger">
+		<div class="bar"></div>
+		<div class="bar2 bar"></div>
+		<div class="bar"></div>
+	</div>
+</nav>
---
+++ admin/sidebar-admin.php
@@ -0,0 +1,7 @@
+<div class="sidebar">
+	<ul>
+	  <li><a class="active" href="g-produits.php">Produits</a></li>
+	  <li><a href="g-utilisateurs.php">Utilisateurs</a></li>
+	  <li><a href="all-reservations.php">Inventaire</a></li>  
+	</ul>
+</div>
